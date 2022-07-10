278799501eac3f6db870b9d254fe309805e2ed10
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2022-07-10 15:53:20 +0700

    main

--- Edubin/app.js
+++ Edubin/app.js
@@ -11,7 +11,7 @@ const userName = document.querySelector("#username");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const passwordConfirm = document.querySelector("#password-confirm");
-const submitBtn= document.querySelector("#register__btn")
+const submitBtn = document.querySelector("#register__btn");
 window.onscroll = function () {
   myFunction();
 };
@@ -60,7 +60,6 @@ function resetSlick($slick_slider, settings) {
     }
   });
 }
-
 
 $(document).ready(function () {
   $(".bannerchild__slider").slick({
@@ -159,29 +158,37 @@ $(document).ready(function () {
   });
 });
 
-// facilityBtn.onclick = function () {
-//   facilityLayer.classList.toggle("active");
-//   navbar.classList.toggle("hidden");
-// };
-// facilityLayerBtn.onclick = function () {
-//   facilityLayer.classList.toggle("active");
-//   navbar.classList.toggle("hidden");
-// };
-// Fetch Link ảnh lỗi
+facilityBtn.onclick = function () {
+  facilityLayer.classList.toggle("active");
+  navbar.classList.toggle("hidden");
+};
+facilityLayerBtn.onclick = function () {
+  facilityLayer.classList.toggle("active");
+  navbar.classList.toggle("hidden");
+};
+// Fetch Courses
 fetch("https://60d4611a61160900173cb070.mockapi.io/courses")
-.then(response => response.json())
-  .then(data => {
-    let html=''
+  .then((response) => response.json())
+  .then((data) => {
     console.log(data);
-    data.map((x)=>{
-      html+=` <div class="courses__slider--item  ">
+    let html = "";
+
+    data.map((x) => {
+      let rate = "";
+      for (let i = 0; i < x.rate; i++) {
+        rate += `<i class="fa-solid fa-star text-primary"></i> `;
+      }
+      for (let i = 0; i < 5 - x.rate; i++) {
+        rate += `<i class="fa-regular fa-star text-primary"></i> `;
+      }
+      html += ` <div class="courses__slider--item  ">
                     <div class="cursor-pointer h-[220px] w-[340px] overflow-hidden ">
                       <img src="${x.image}" class="  hover:scale-110 w-[340px] duration-500 " alt="">
                     </div>
                     <div class="courses__content p-8 w-[340px] bg-lighttext cursor-pointer">
                       <div class="flex">
-                        <div class="rate  text-primary ">
-
+                        <div class="rate text-primary ">
+                           ${rate}
                         </div>
                         <p class="font-semibold px-3 text-textdarkcolor">${x.rate}<p class="font-semibold  text-textdarkcolor" >(${x.rate_quantity})</p> </p>
 
@@ -193,31 +200,26 @@ fetch("https://60d4611a61160900173cb070.mockapi.io/courses")
                       <p class="pr-3" >${x.total_enrolled}</p>
                       <i class="fa-regular fa-clock pt-1 pr-2"></i>
                       <p>${x.duration}</p>
-
                      </div>
-
                      <div class=" flex justify-between py-5">
                       <div class="flex">
                         <img src="./assets/image/teacher_1.jpg" class="w-[40px] h-[40px] rounded-[50%]" alt="">
-                      <p class="pl-2 "> by  <h2 class="hover:text-primary font-bold duration-500  px-1 ">Tom Hardy</h2> in  <h2 class="hover:text-primary font-bold duration-500  px-1 text-infor">Tom Hardy</h2> </p>
+                      <p class="pl-2 "> by  <h2 class="hover:text-primary font-bold duration-500 text-infor  px-1 ">Tom Hardy</h2> in  <h2 class="hover:text-primary font-bold duration-500  px-1 text-infor">Tom Hardy</h2> </p>
                       </div>
                      </div>
                      <div class="w-[100%] h-[1px] bg-gray-200"></div>
                       <div class="courses__footer flex   py-3">
-
                           <h2 class="hover:text-primary font-bold duration-500 text-md px-1 mr-[100px] ">${x.price}</h2>
-
                         <div class="text-textdarkcolor flex ml-2">
                           <i class="text-primary fa-solid fa-cart-arrow-down pt-1 pr-1"></i>
                           <p class="font-semibold">Get Enrolled</p>
                         </div>
-
                       </div>
 
                   </div>
-                  </div>`
-    })
-    coursesSlider.innerHTML=html;
+                  </div>`;
+    });
+    coursesSlider.innerHTML = html;
 
     $(document).ready(function () {
       $(".courses__slider").slick({
@@ -226,7 +228,7 @@ fetch("https://60d4611a61160900173cb070.mockapi.io/courses")
         infinite: true,
         arrows: true,
         draggable: false,
-    
+
         prevArrow: $(".prev__courses"),
         nextArrow: $(".next__courses"),
         responsive: [
@@ -239,69 +241,64 @@ fetch("https://60d4611a61160900173cb070.mockapi.io/courses")
             },
           },
         ],
-    
+
         autoplay: true,
         autoplaySpeed: 1000,
       });
     });
   });
- 
+
 //Register
 
-let user={}
+let user = {};
 
-function object(){
-  user.firstName=firstName.value
-  user.lastName=lastName.value
-  user.username=userName.value
-  user.email=email.value
-  user.password=password.value
-  user.passwordConfirm=passwordConfirm.value
+function object() {
+  user.firstName = firstName.value;
+  user.lastName = lastName.value;
+  user.username = userName.value;
+  user.email = email.value;
+  user.password = password.value;
+  user.passwordConfirm = passwordConfirm.value;
 }
 function validate() {
-      
-  if( document.myForm.lastname.value == "" ) {
-     alert( "Please provide your last name!" );
-     document.myForm.lastname.focus() ;
-     return false;
-  }
-  if( document.myForm.firstname.value == "" ) {
-    alert( "Please provide your first name!" );
-    document.myForm.firstname.focus() ;
+  if (document.myForm.lastname.value == "") {
+    alert("Please provide your last name!");
+    document.myForm.lastname.focus();
     return false;
- }
-  if( document.myForm.email.value == "" ) {
-     alert( "Please provide your Email!" );
-     document.myForm.email.focus() ;
-     return false;
   }
-  if( document.myForm.username.value == "" ) {
-    alert( "Please provide your username!" );
-    document.myForm.username.focus() ;
+  if (document.myForm.firstname.value == "") {
+    alert("Please provide your first name!");
+    document.myForm.firstname.focus();
     return false;
- }
-  if( document.myForm.password.value == "" ||  
-     document.myForm.password.value.length < 8 ) {
-     
-     alert( "Please provide a password in the format #####." );
-     document.myForm.password.focus() ;
-     return false;
   }
-  if( document.myForm.passwordconfirm.value != document.myForm.password.value) {
-     alert( "Please provide the same password !" );
-     return false;
+  if (document.myForm.email.value == "") {
+    alert("Please provide your Email!");
+    document.myForm.email.focus();
+    return false;
   }
-  return( true );
+  if (document.myForm.username.value == "") {
+    alert("Please provide your username!");
+    document.myForm.username.focus();
+    return false;
+  }
+  if (
+    document.myForm.password.value == "" ||
+    document.myForm.password.value.length < 8
+  ) {
+    alert("Please provide a password in the format #####.");
+    document.myForm.password.focus();
+    return false;
+  }
+  if (document.myForm.passwordconfirm.value != document.myForm.password.value) {
+    alert("Please provide the same password !");
+    return false;
+  }
+  return true;
 }
 
-submitBtn.onclick=()=>{
-  if(!validate()){
-    object()
-    localStorage.setItem('user', JSON.stringify(user));
+submitBtn.onclick = () => {
+  if (!validate()) {
+    object();
+    localStorage.setItem("user", JSON.stringify(user));
   }
-
-}
- 
-
-
-
+};
